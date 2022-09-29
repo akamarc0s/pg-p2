af98c39310de010a161b24c0089e03dd483b99bd
Marcos Antonio de Santana Junior <marcos.santana@estudante.ufscar.br>
2022-09-29 18:15:10 -0300

    Add:JS Libs/Files to be used in the project and a HTML file to project the 3D objects

---
+++ js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function main() {
+    // Get A WebGL context
+    /** @type {HTMLCanvasElement} */
+    var canvas = document.querySelector("#canvas");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+}
+main();
